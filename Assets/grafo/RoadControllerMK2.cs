94ed7fcb9784a6f3fb5059b554c616a33062b835
Geromin <luciano.geronimo.fnord@gmail.com>
2018-11-24 14:15:28 -0200

    Primeira versão da geração das interseções.

--- Assets/grafo/RoadControllerMK2.cs
+++ Assets/grafo/RoadControllerMK2.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class RoadControllerMK2 : MonoBehaviour { 
 
@@ -14,6 +15,7 @@ public class RoadControllerMK2 : MonoBehaviour {
     private Vector3 RoadFirstPoint, RoadSecondPoint;
     private List<LineSegment> segments = new List<LineSegment>();
     private List<Vector3> intersections = new List<Vector3>();
+    
 	// Use this for initialization
 	void Start () {
         ///Se não estiver pondo uma rua passa a poder por o 1o ponto
@@ -45,113 +47,136 @@ public class RoadControllerMK2 : MonoBehaviour {
             RoadSecondPoint = pt;
             LayRoadState = LayRoadNotLaying;
             segments.Add(new LineSegment(RoadFirstPoint, RoadSecondPoint));
+            BuildGraph();
+        }
+    }
+
+    private void BuildGraph()
+    {
+        intersections.Clear();
+        FindIntersections();
+        //TODO: As interseções são as encruzilhadas com as quais eu montarei o grafo de ruas, montar o grafo de ruas agora.
+    }
+
+    private void FindIntersections()
+    {
+        //Mapeia os pontos pro 2d descartando o y
+        //TODO: isso aqui não é n x n. Quando eu testo o segmento i com o segmento i+1 eu tb testo i+1 com i, então não preciso executar tantas vezes
+        //qto tá executando.
+        var _2dSegments = segments.Select<LineSegment, LineSegment2d>(s => new LineSegment2d(s.Point1, s.Point2)).ToList();
+        for (int i = 0; i < _2dSegments.Count(); i++)
+        {
+            for (int j = 0; j < _2dSegments.Count(); j++)
+            {
+                if (_2dSegments[i] == _2dSegments[j])
+                    continue;
+                var s1 = _2dSegments[i];
+                var s2 = _2dSegments[j];
+                Vector2 intersectionIn2d;
+                bool doesIntersect = Line2dIntersectionService.LineSegmentsIntersection(s1.Point1, s1.Point2, s2.Point1, s2.Point2, out intersectionIn2d);
+                if (doesIntersect)
+                {
+                    //A posição paramétrica no segmento 1 do ponto de interseção. Poderia ser no segmento 2 mas tanto faz, a posição espacial vai ser a mesma.
+                    float gammaP2d = (intersectionIn2d - s1.Point1).magnitude / (s1.Point2 - s1.Point1).magnitude;
+                    //Relação entre as magnitudes do vetor2d e do vetor3d que corresponde a ele
+                    var R = (s1.Point2 - s1.Point1).magnitude / (segments[i].Point2 - segments[i].Point1).magnitude;
+                    //o parâmetro no vetor 3d
+                    var gammaP3d = gammaP2d * R;
+                    //o ponto da interseção no 3d
+                    var p3d = segments[i].Point1 + gammaP3d * (segments[i].Point2 - segments[i].Point1);
+                    intersections.Add(p3d);
+                }
+            }
         }
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.magenta;
+        Gizmos.color = Color.blue;
         foreach(var s in segments)
         {
             Gizmos.DrawLine(s.Point1, s.Point2);
+        }
+        Gizmos.color = Color.red;
+        foreach(var i in intersections)
+        {
+            Gizmos.DrawSphere(i, 0.25f);
         }
         
     }
 
 	// Update is called once per frame
 	void Update () {
-        //Vector3 v1 = new Vector3(0, 1.2f, 0);
-        //h.Add(v1);
-        //Para cada segmento testa interseção com todos os outros segmentos
-        //Isso vai ser uma coisa O(n)=n^2 mas no momento não vejo jeito diferente
-        foreach(var currentSegment in segments)
-        {
-            foreach(var otherSegment in segments)//Loop interno
-            {
-                if (otherSegment.Equals(currentSegment))
-                    continue;
-                Vector3 outPoint1 = Vector3.zero;
-                Vector3 outPoint2 = Vector3.zero;
-                var intersects = CalculateLineLineIntersection(currentSegment.Point1, currentSegment.Point2,
-                    otherSegment.Point1, otherSegment.Point2, out outPoint1, out outPoint2);
-                if (intersects)
-                {
-                    if (!IsNotPresentInIntersectionList(outPoint1))
-                        intersections.Add(outPoint1);
-                }
-            }
-        }
-
     }
 
-    private bool IsNotPresentInIntersectionList(Vector3 outPoint1)
-    {
-        float EPSILON = 0.01f;
-        foreach(var i in intersections)
-        {
-            bool x = (Mathf.Abs(outPoint1.x - i.x)) < EPSILON;
-            bool y = (Mathf.Abs(outPoint1.y - i.y)) < EPSILON;
-            bool z = (Mathf.Abs(outPoint1.z - i.z)) < EPSILON;
-            return x && y && z;
-        }
-        return false;
-    }
+    //private bool IsNotPresentInIntersectionList(Vector3 outPoint1)
+    //{
+    //    float EPSILON = 0.01f;
+    //    foreach(var i in intersections)
+    //    {
+    //        bool x = (Mathf.Abs(outPoint1.x - i.x)) < EPSILON;
+    //        bool y = (Mathf.Abs(outPoint1.y - i.y)) < EPSILON;
+    //        bool z = (Mathf.Abs(outPoint1.z - i.z)) < EPSILON;
+    //        return x && y && z;
+    //    }
+    //    return false;
+    //}
 
-    /// <summary>
-    /// http://paulbourke.net/geometry/pointlineplane/calclineline.cs
-    /// Calculates the intersection line segment between 2 lines (not segments).
-    /// Returns false if no solution can be found.
-    /// </summary>
-    /// <returns></returns>
-    public static bool CalculateLineLineIntersection(Vector3 line1Point1, Vector3 line1Point2,
-        Vector3 line2Point1, Vector3 line2Point2, out Vector3 resultSegmentPoint1, out Vector3 resultSegmentPoint2)
-    {
-        // Algorithm is ported from the C algorithm of 
-        // Paul Bourke at http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline3d/
-        resultSegmentPoint1 = Vector3.zero;
-        resultSegmentPoint2 = Vector3.zero;
-        float EPSILON = 0.001f;
+    ///// <summary>
+    ///// http://paulbourke.net/geometry/pointlineplane/calclineline.cs
+    ///// Calculates the intersection line segment between 2 lines (not segments).
+    ///// Returns false if no solution can be found.
+    ///// </summary>
+    ///// <returns></returns>
+    //public static bool CalculateLineLineIntersection(Vector3 line1Point1, Vector3 line1Point2,
+    //    Vector3 line2Point1, Vector3 line2Point2, out Vector3 resultSegmentPoint1, out Vector3 resultSegmentPoint2)
+    //{
+    //    // Algorithm is ported from the C algorithm of 
+    //    // Paul Bourke at http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline3d/
+    //    resultSegmentPoint1 = Vector3.zero;
+    //    resultSegmentPoint2 = Vector3.zero;
+    //    float EPSILON = 0.001f;
 
-        Vector3 p1 = line1Point1;
-        Vector3 p2 = line1Point2;
-        Vector3 p3 = line2Point1;
-        Vector3 p4 = line2Point2;
-        Vector3 p13 = p1 - p3;
-        Vector3 p43 = p4 - p3;
+    //    Vector3 p1 = line1Point1;
+    //    Vector3 p2 = line1Point2;
+    //    Vector3 p3 = line2Point1;
+    //    Vector3 p4 = line2Point2;
+    //    Vector3 p13 = p1 - p3;
+    //    Vector3 p43 = p4 - p3;
         
-        if (p43.sqrMagnitude < EPSILON)
-        {
-            return false;
-        }
-        Vector3 p21 = p2 - p1;
-        if (p21.sqrMagnitude < EPSILON)
-        {
-            return false;
-        }
+    //    if (p43.sqrMagnitude < EPSILON)
+    //    {
+    //        return false;
+    //    }
+    //    Vector3 p21 = p2 - p1;
+    //    if (p21.sqrMagnitude < EPSILON)
+    //    {
+    //        return false;
+    //    }
 
-        double d1343 = p13.x * (double)p43.x + (double)p13.y * p43.y + (double)p13.z * p43.z;
-        double d4321 = p43.x * (double)p21.x + (double)p43.y * p21.y + (double)p43.z * p21.z;
-        double d1321 = p13.x * (double)p21.x + (double)p13.y * p21.y + (double)p13.z * p21.z;
-        double d4343 = p43.x * (double)p43.x + (double)p43.y * p43.y + (double)p43.z * p43.z;
-        double d2121 = p21.x * (double)p21.x + (double)p21.y * p21.y + (double)p21.z * p21.z;
+    //    double d1343 = p13.x * (double)p43.x + (double)p13.y * p43.y + (double)p13.z * p43.z;
+    //    double d4321 = p43.x * (double)p21.x + (double)p43.y * p21.y + (double)p43.z * p21.z;
+    //    double d1321 = p13.x * (double)p21.x + (double)p13.y * p21.y + (double)p13.z * p21.z;
+    //    double d4343 = p43.x * (double)p43.x + (double)p43.y * p43.y + (double)p43.z * p43.z;
+    //    double d2121 = p21.x * (double)p21.x + (double)p21.y * p21.y + (double)p21.z * p21.z;
 
-        double denom = d2121 * d4343 - d4321 * d4321;
-        if (Math.Abs(denom) < EPSILON)
-        {
-            return false;
-        }
-        double numer = d1343 * d4321 - d1321 * d4343;
+    //    double denom = d2121 * d4343 - d4321 * d4321;
+    //    if (Math.Abs(denom) < EPSILON)
+    //    {
+    //        return false;
+    //    }
+    //    double numer = d1343 * d4321 - d1321 * d4343;
 
-        double mua = numer / denom;
-        double mub = (d1343 + d4321 * (mua)) / d4343;
+    //    double mua = numer / denom;
+    //    double mub = (d1343 + d4321 * (mua)) / d4343;
 
-        resultSegmentPoint1.x = (float)(p1.x + mua * p21.x);
-        resultSegmentPoint1.y = (float)(p1.y + mua * p21.y);
-        resultSegmentPoint1.z = (float)(p1.z + mua * p21.z);
-        resultSegmentPoint2.x = (float)(p3.x + mub * p43.x);
-        resultSegmentPoint2.y = (float)(p3.y + mub * p43.y);
-        resultSegmentPoint2.z = (float)(p3.z + mub * p43.z);
+    //    resultSegmentPoint1.x = (float)(p1.x + mua * p21.x);
+    //    resultSegmentPoint1.y = (float)(p1.y + mua * p21.y);
+    //    resultSegmentPoint1.z = (float)(p1.z + mua * p21.z);
+    //    resultSegmentPoint2.x = (float)(p3.x + mub * p43.x);
+    //    resultSegmentPoint2.y = (float)(p3.y + mub * p43.y);
+    //    resultSegmentPoint2.z = (float)(p3.z + mub * p43.z);
 
-        return true;
-    }
+    //    return true;
+    //}
 }
--- Assets/grafo/RoadGraph.cs
+++ Assets/grafo/RoadGraph.cs
@@ -2,6 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Line2dIntersectionService
+{
+    //De https://github.com/setchi/Unity-LineSegmentsIntersection/blob/master/Assets/LineSegmentIntersection/Scripts/Math2d.cs
+    public static bool LineSegmentsIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, out Vector2 intersection)
+    {
+        intersection = Vector2.zero;
+
+        var d = (p2.x - p1.x) * (p4.y - p3.y) - (p2.y - p1.y) * (p4.x - p3.x);
+
+        if (d == 0.0f)
+        {
+            return false;
+        }
+
+        var u = ((p3.x - p1.x) * (p4.y - p3.y) - (p3.y - p1.y) * (p4.x - p3.x)) / d;
+        var v = ((p3.x - p1.x) * (p2.y - p1.y) - (p3.y - p1.y) * (p2.x - p1.x)) / d;
+
+        if (u < 0.0f || u > 1.0f || v < 0.0f || v > 1.0f)
+        {
+            return false;
+        }
+
+        intersection.x = p1.x + u * (p2.x - p1.x);
+        intersection.y = p1.y + u * (p2.y - p1.y);
+
+        return true;
+    }
+}
+
+public class LineSegment2d
+{
+    public Vector2 Point1 { get; }
+    public Vector2 Point2 { get; }
+    //Esse dado veio originalmente de dois pontos 3d. Eu guardo a magnitude deles pq vou 
+    //precisar de ter a relação entre a magnitude deles no 2d e no 3d pra quando eu andar
+    //em um poder andar corretamente no outro.
+    public float OriginalMagnitude { get; }
+
+    /// <summary>
+    ///Descarta a informação de y e fica só com xz,
+    ///X->X
+    ///Y->-
+    ///Z->Y
+    /// </summary>
+    /// <param name="p0"></param>
+    /// <param name="p1"></param>
+    public LineSegment2d(Vector3 p0, Vector3 p1)
+    {
+        Vector2 ponto1 = new Vector2(p0.x, p0.z);
+        Vector2 ponto2 = new Vector2(p1.x, p1.z);
+        Point1 = ponto1;
+        Point2 = ponto2;
+        OriginalMagnitude = (p1 - p0).magnitude;
+    }
+}
+
 public class LineSegment
 {
     public Vector3 Point1 { get; }
@@ -12,18 +68,5 @@ public class LineSegment
         Point2 = p2;
     }
 
-    public override bool Equals(object obj)
-    {
-        if (obj is LineSegment)
-        {
-            return ((LineSegment)obj).Point1.Equals(this.Point1) && ((LineSegment)obj).Point2.Equals(this.Point2);
-        }
-        else return false;
-    }
-
-    public override int GetHashCode()
-    {
-        return Point1.GetHashCode() * Point2.GetHashCode() * 91;
-    }
 }
 
